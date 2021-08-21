09c66541ed9b3356035ac30fa2bb469580c5d55c
Zeragamba <Stephen-356@hotmail.com>
2021-08-21 16:38:26 -0400

    add space between edge tracker buttons

--- src/System/Edge/EdgeTracker.tsx
+++ src/System/Edge/EdgeTracker.tsx
@@ -24,12 +24,12 @@ export const EdgeTracker: FC<EdgeTrackerProps> = ({
   return (
     <Box>
       <Typography variant={'h6'}>Edge</Typography>
-      <Box sx={{ display: 'flex', textAlign: 'center' }}>
+      <Box sx={{ display: 'flex', textAlign: 'center', gap: 0.5}}>
         <Box sx={{ flexGrow: 0.5 }}>
           <TrackCell onClick={() => setCurValue(0)}>0</TrackCell>
         </Box>
         {new Array(7).fill(null).map((_, index) => (
-          <Box key={index} sx={{ flexGrow: 1 }}>
+          <Box key={index} sx={{ flexGrow: 1}}>
             <TrackCell filled={index < curValue} onClick={() => onToggleCell(index + 1)}>
               {index + 1}
             </TrackCell>
