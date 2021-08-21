c3fd5305b1e58a226978dfca53f24525f6d255ad
Zeragamba <Stephen-356@hotmail.com>
2021-08-21 13:39:07 -0400

    optionally display Autosoft avail and cost

--- src/Gear/Software/Autosoft/AutosoftsList.tsx
+++ src/Gear/Software/Autosoft/AutosoftsList.tsx
@@ -4,21 +4,36 @@ import { Box } from '@material-ui/core'
 import { FC } from 'react'
 
 import { AttributeBlock } from '../../../UI/AttributeBlock'
+import { StatBlock } from '../../../UI/StatBlock'
 import { GearId } from '../../GearData'
+import { AvailabilityStat } from '../../Stats/Availability'
+import { CostStat } from '../../Stats/Cost'
 import { AutosoftAttrNames, AutosoftData } from './AutosoftData'
 
 interface AutosoftsListProps {
   autosofts: AutosoftData[]
   slavedIds?: GearId[]
+  withAvail?: boolean
+  withCost?: boolean
 }
 
 export const AutosoftsList: FC<AutosoftsListProps> = ({
   autosofts,
   slavedIds,
+  withAvail = false,
+  withCost = false,
 }) => {
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
-      {autosofts.map(autosoft => <AutosoftListItem key={autosoft.id} autosoft={autosoft} slavedIds={slavedIds} />)}
+      {autosofts.map(autosoft => (
+        <AutosoftListItem
+          key={autosoft.id}
+          autosoft={autosoft}
+          slavedIds={slavedIds}
+          withAvail={withAvail}
+          withCost={withCost}
+        />
+      ))}
     </Box>
   )
 }
@@ -26,11 +41,15 @@ export const AutosoftsList: FC<AutosoftsListProps> = ({
 interface AutosoftListItemProps {
   autosoft: AutosoftData
   slavedIds?: GearId[]
+  withAvail?: boolean
+  withCost?: boolean
 }
 
 const AutosoftListItem: FC<AutosoftListItemProps> = ({
   autosoft,
   slavedIds,
+  withAvail = false,
+  withCost = false,
 }) => {
   return (
     <Box>
@@ -48,6 +67,14 @@ const AutosoftListItem: FC<AutosoftListItemProps> = ({
         <Box sx={{ flexGrow: 1 }}>
           <AttributeBlock attributes={autosoft.attributes} names={AutosoftAttrNames} />
         </Box>
+        {(withAvail || withCost) && (
+          <Box sx={{ marginLeft: 1 }}>
+            <StatBlock>
+              {withAvail && <AvailabilityStat avail={autosoft.avail} />}
+              {withCost && <CostStat cost={autosoft.cost} />}
+            </StatBlock>
+          </Box>
+        )}
       </Box>
     </Box>
   )
