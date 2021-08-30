5a08a90f1f38c422c81c10d22dd03273b2359dc0
Zeragamba <Stephen-356@hotmail.com>
2021-08-30 00:37:41 -0400

    add Users table

---
+++ server/spec/requests/user_spec.rb
@@ -0,0 +1,69 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :request do
+  describe "GET /user" do
+    let(:users) {
+      [
+        User.new({ username: "User 1" }),
+        User.new({ username: "User 2" }),
+        User.new({ username: "User 3" }),
+      ]
+    }
+
+    before { users.each(&:save) }
+
+    it "returns http success" do
+      get "/user"
+
+      expect(response).to have_http_status(:success)
+    end
+
+    it "contains count of records" do
+      get "/user"
+
+      body = JSON.parse(response.body)
+      expect(body["count"]).to eq(users.size)
+    end
+
+    it "contains list of all users" do
+      get "/user"
+
+      body = JSON.parse(response.body)
+      usernames = body["users"].map { |user| user["username"] }
+      users.each { |user| expect(usernames).to include(user.username) }
+    end
+
+    context "when there are no users" do
+      let(:users) { [] }
+
+      it "returns success" do
+        get "/user"
+
+        expect(response).to have_http_status(:success)
+
+        body = JSON.parse(response.body)
+        expect(body["count"]).to eq(0)
+        expect(body["users"]).to eq([])
+      end
+    end
+  end
+
+  describe "GET /user/:id" do
+    let(:user) { User.create({ username: "User" }) }
+
+    it "returns http success" do
+      get "/user/#{user.id}"
+
+      expect(response).to have_http_status(:success)
+    end
+
+    context "when user does not exist" do
+
+      it "returns 404" do
+        get "/user/null-user"
+
+        expect(response).to have_http_status(:not_found)
+      end
+    end
+  end
+end
