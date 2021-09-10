b35786fb0f9e2592883288b1e3e1164a2e3da108
Zeragamba <Stephen-356@hotmail.com>
2021-09-10 10:51:57 -0400

    add login/out routes to server

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  post 'login', to: 'auth#login'
+  post '/login', to: 'auth#login'
+  post '/logout', to: 'auth#logout'
 
-  get 'user', to: 'user#view'
-  patch 'user', to: 'user#update'
+  get '/user', to: 'user#view'
+  patch '/user', to: 'user#update'
 
   resources :characters
   resources :gear
