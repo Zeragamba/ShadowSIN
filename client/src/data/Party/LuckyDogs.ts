00fba8dd7cc339d15a95019bb5869abad238e903
Stephen A. Wilson <Stephen-356@hotmail.com>
2021-12-19 16:04:48 -0500

    add character ids to party

--- client/src/data/Party/LuckyDogs.ts
+++ client/src/data/Party/LuckyDogs.ts
@@ -1,13 +1,25 @@
 import {GearData} from '../../Gear/GearData'
 import {BalanceLog} from '../../System/BalanceLog'
+import {Artemis} from '../Characters/Artemis'
+import {Silicus} from '../Characters/Silicus'
+import {Spike} from '../Characters/Spike'
+import {Xendris} from '../Characters/Xendris'
 
 interface Party {
+  characterIds: string[]
   nuyen: BalanceLog
   gear: GearData[]
 }
 
 export const LuckyDogs: Party = {
-  'nuyen': [
+  characterIds: [
+    Artemis.bio.name,
+    Silicus.bio.name,
+    Spike.bio.name,
+    Xendris.bio.name,
+  ],
+
+  nuyen: [
     {
       date: '2021-12-19T16:00',
       value: 40_000,
